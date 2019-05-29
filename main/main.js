409054f0b370c48e20d238dc94c3b9e5d8bdac3f
chatc <zys_819@126.com>
2019-05-29 15:51:47 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,8 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    dis = inputs['distance']
+    park = inputs['parkTime']
+    var ans = 6
+    if(dis>=2) ans+=(dis-2)*0.8
+    if(dis>=8) ans+=(dis-8)*0.4
+    return Math.round(ans+park*0.25)
 };
